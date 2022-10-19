d4378aa64fa200f8849f8cacff48249c5c56994c
022by <46434255+pplulee@users.noreply.github.com>
2022-10-19 17:30:52 +0100

    更新后端安装脚本

--- backend/install_unblocker.sh
+++ backend/install_unblocker.sh
@@ -3,21 +3,45 @@ install_path="/opt/apple_auto"
 
 echo "以全新方式管理你的 Apple ID，基于密保问题的自动化Apple ID检测&解锁程序程序"
 echo "项目地址：github.com/pplulee/appleid_auto"
-echo "使用时请确保本机已安装Python3和Docker"
+echo "项目交流TG群：@appleunblocker"
+echo "使用时请确保本机已安装Python3.6+ pip3 Docker"
 echo "==============================================================="
 if python3 -V >/dev/null 2>&1; then
     echo "Python3已安装"
     python_path=$(which python3)
     echo "Python3路径：$python_path"
 else
-    echo "Python3未安装，请先安装Python3"
-    exit 1
+    echo "Python3未安装，开始安装……"
+    if [ -f /etc/debian_version ]; then
+        apt update && apt -y install python3 python3-pip
+    elif [ -f /etc/redhat-release ]; then
+        yum -y install python3 python3-pip
+    else
+       echo "无法检测到当前系统，已退出"
+       exit;
+    fi
+fi
+if pip3 >/dev/null 2>&1; then
+    echo "pip3已安装"
+else
+    echo "pip3未安装，开始安装……"
+    if [ -f /etc/debian_version ]; then
+        apt update && apt -y install python3-pip
+    elif [ -f /etc/redhat-release ]; then
+        yum -y install python3-pip
+    else
+       echo "无法检测到当前系统，已退出"
+       exit;
+    fi
+    echo "pip3安装完成"
 fi
 if docker >/dev/null 2>&1; then
     echo "Docker已安装"
 else
-    echo "Docker未安装，请先安装Docker"
-    exit 1
+    echo "Docker未安装，开始安装……"
+    docker version > /dev/null || curl -fsSL get.docker.com | bash
+    systemctl enable docker && systemctl restart docker
+    echo "Docker安装完成"
 fi
 echo "开始安装Apple_Auto后端"
 echo "请输入API URL（http://xxx.xxx）"
