e116034b5605b30f3037c5128e733286273e4664
pplulee <46434255+pplulee@users.noreply.github.com>
2023-03-03 06:16:57 +0000

    删除登录时选项框判断

--- backend/unblocker/main.py
+++ backend/unblocker/main.py
@@ -271,14 +271,8 @@ class ID:
                                                                                  "/html/body/div[1]/iforgot-v2/app-container/div/iforgot-body/global-v2/div/idms-flow/div/forgot-password/div/div/div[1]/idms-step/div/div/div/div[2]/div/div[1]/div[1]/div/idms-textbox/idms-error-wrapper/div/idms-error/div/div/span"))).get_attribute(
                 "innerHTML")
         except BaseException:
-            try:
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "form-radiobutton")))
-            except BaseException:
-                logger.error("超时无法获取页面")
-                return False
-            else:
-                logger.info("登录成功")
-                return True
+            logger.info("登录成功")
+            return True
         else:
             logger.error(f"无法处理请求，可能是账号失效或服务器IP被拉黑\n错误信息：{msg.strip()}")
             api.update_message(self.username, "解锁登录失败，可能是账号失效或服务器IP被拉黑，具体请查看后端日志")
