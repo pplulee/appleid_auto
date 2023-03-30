19d87678ffd4b5adc1b20dcacb9ced626b22a7ad
022by <46434255+pplulee@users.noreply.github.com>
2023-03-31 00:12:48 +0100

    fix: 更新文件链接

--- backend/install_unblocker.sh
+++ backend/install_unblocker.sh
@@ -108,8 +108,8 @@ rm -rf install_unblocker
 mkdir install_unblocker
 cd install_unblocker
 echo "开始下载文件…… | Start downloading files..."
-wget https://raw.githubusercontent.com/pplulee/appleid_auto/main/backend/requirements.txt -O requirements.txt
-wget https://raw.githubusercontent.com/pplulee/appleid_auto/main/backend/unblocker_manager.py -O unblocker_manager.py
+wget https://raw.githubusercontent.com/pplulee/appleid_auto/backend/backend/requirements.txt -O requirements.txt
+wget https://raw.githubusercontent.com/pplulee/appleid_auto/backend/backend/unblocker_manager.py -O unblocker_manager.py
 SERVICE_FILE="[Unit]
 Description=appleauto
 Wants=network.target
