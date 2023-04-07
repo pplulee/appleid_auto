070cc2aedd88e8bb3b71eb5f66d78951564f3257
022by <46434255+pplulee@users.noreply.github.com>
2023-04-07 10:52:53 +0100

    fix: API返回增加状态

--- app/middleware/Api.php
+++ app/middleware/Api.php
@@ -10,15 +10,15 @@ class Api
     public function handle($request, Closure $next)
     {
         if (!$request->isPost()) {
-            return json(["code" => 403, "msg" => "请求方式错误"]);
+            return json(["code" => 403, "msg" => "请求方式错误", "status" => false]);
         }
         $key = $request->header('key');
         if (!$key || $key == "") {
-            return json(['code' => 401, 'msg' => '未提供密钥']);
+            return json(['code' => 401, 'msg' => '未提供密钥', "status" => false]);
         } else {
             $apikey = env('API_KEY');
             if ($key != $apikey) {
-                return json(['code' => 403, 'msg' => '密钥错误']);
+                return json(['code' => 403, 'msg' => '密钥错误', "status" => false]);
             } else {
                 return $next($request);
             }
